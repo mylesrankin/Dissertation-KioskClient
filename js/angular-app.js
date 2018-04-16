32afeae2602915747f6756b8b475bf6bd8e63ace
mylesrankin <myles.rankin@sky.com>
2018-04-16 21:32:08 +0100

    Added heartbeats and impression reporting

--- js/angular-app.js
+++ js/angular-app.js
@@ -1,14 +1,16 @@
 var app = angular.module('kioskApp', []);
 
 
-app.controller('kioskFrontScreen', function($scope, $timeout, $interval) {
-    localStorage.hardwareid = 'test'
+app.controller('kioskFrontScreen', function($scope, $timeout, $interval, $http) {
+    new Fingerprint2().get(function(result, components) {
+        localStorage.hardwareid = result
+    })
     $interval(function(){
         $.ajax({
             url: "http://localhost:3000/screen/heartbeat",
             type: "POST",
             beforeSend: function (request) {
-                request.setRequestHeader("hardwareid", "test");
+                request.setRequestHeader("hardwareid", localStorage.hardwareid);
             },
             success: function(){
                 console.log('Heartbeat sent.')
@@ -23,7 +25,7 @@ app.controller('kioskFrontScreen', function($scope, $timeout, $interval) {
     $.ajax({
         type: "GET",
         beforeSend: function (request) {
-            request.setRequestHeader("hardwareid", "test");
+            request.setRequestHeader("hardwareid", localStorage.hardwareid);
         },
         url: "http://localhost:3000/screen/adverts",
         success: function(data){
@@ -47,6 +49,48 @@ app.controller('kioskFrontScreen', function($scope, $timeout, $interval) {
             }
             $scope.$apply();
         }
-    });
+    })
+
+    $scope.submitResponse = function(advertID, previousBtn){
+        console.log(advertID)
+        console.log(document.getElementById("advert-"+advertID+"-form-name").value)
+        $http({
+            url: "http://127.0.0.1:3000/response",
+            method: "POST",
+            headers: {"hardwareid":localStorage.hardwareid},
+            data: {
+                "Name": document.getElementById("advert-" + advertID + "-form-name").value,
+                "Email": document.getElementById("advert-" + advertID + "-form-email").value,
+                "OriginAdvertID": advertID
+            }
+            }).then(function successCallback(res){
+                document.getElementById("advert-"+advertID+"-btn").innerHTML = "Response recorded, thank you."
+                document.getElementById("advert-"+advertID+"-form-name").disabled = "true"
+                document.getElementById("advert-"+advertID+"-form-email").disabled = "true"
+                document.getElementById("advert-"+advertID+"-btn").className = "btn btn-lg btn-danger btn-block"
+
+                $timeout(function(){
+                    document.getElementById("advert-"+advertID+"-btn").innerHTML = previousBtn
+                    document.getElementById("advert-"+advertID+"-form-name").disabled = ""
+                    document.getElementById("advert-"+advertID+"-form-email").disabled = ""
+                    document.getElementById("advert-"+advertID+"-btn").className = "btn btn-lg btn-success btn-block"
+                    }, 5000)
+            }, function errorCallback(res){
+                    console.log(res)
+            })
+        }
+
+    $scope.incrementImpression = function(advertID){
+        console.log("Incrementing "+advertID)
+        $http({
+            url: "http://127.0.0.1:3000/screen/impression",
+            method: "POST",
+            headers: {"hardwareid": localStorage.hardwareid},
+            data: {
+                "advertid": advertID
+            }
+        })
+    }
+
 
 });
